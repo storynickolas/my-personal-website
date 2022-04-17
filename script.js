ee5706344da0d826a7c27f8682af7bbfc691e198
Nick Story <storynickolasl@gmail.com>
2022-04-17 09:55:22 -0400

    Adjust HTML layout

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
+function test() {
+  alert('button test')
+}
+let background = 1
+
+function add() {
+  background < 5 ? background++ : background;
+  document.getElementById('photo').src = `image${background}.jpg`
+  console.log(background)
+}
+
+function subtract() {
+  background >= 2 ? background-- : background;
+  document.getElementById('photo').src = `image${background}.jpg`
+  console.log(background)
+}
+
+
+document.getElementById('photo').src = `image1.jpg`
+
+
 document.querySelector("body").appendChild(h2);
+
+
+
+
+
+
